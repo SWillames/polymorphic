a6d59b19765a62186f3a046d5031af40feb3a357
SWillames <sergiowoc@gmail.com>
2019-11-05 03:36:00 -0300

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+@pedro = User.create(email: 'pedro@email.com', name:'Pedro', password: '12345678', password_confirmation: '12345678') 
+
+@jon = User.create(email: 'jon@email.com', name:'jon', password: '12345678', password_confirmation: '12345678') 
+
+@post_movie = Post.create(user_id: @pedro.id, post: "Hoje assisti o filme A start is Born") 
+
+@comment = Comment.create(user_id: @jon.id, comment: "Esse filme é muito bom!", commentable: @post_movie) 
+
+Comment.create(user_id: @pedro.id, comment: "Achei o filme muito bom mas o do freddie é bem melhor", commentable: @comment) 
+
+Comment.create(user_id: @jon.id, comment: "Não assisti o filme do freddie", commentable: @comment) 
+
+Comment.create(user_id: @pedro.id, comment: "Pois deveria assistir é um filme sensacional", commentable: @comment) 
+
+Comment.create(user_id: @jon.id, comment: "Irei assistir", commentable: @comment)
